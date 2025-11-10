d29e0c6a03e4e9493fad16320f13777c50b9eb34
Anna_Gorova <koshka20.03.19@gmail.com>
2025-11-10 17:26:57 +0200

    Controller + Views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,11 +6,11 @@ namespace WebAppStocker_1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+       
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+           
         }
 
         public IActionResult Index()
@@ -18,15 +18,5 @@ namespace WebAppStocker_1.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppStocker_1.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Services() 
+        {
+            ViewData["ActivePage"] = "Services";
+            return View();
+        }
+    }
+}
